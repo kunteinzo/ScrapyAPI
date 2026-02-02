58cf48b15702c593dff85f8b29c154af6247ccb9
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2026-02-02 15:21:39 +0630

    celery for bg task

---
+++ celery_app.py
@@ -0,0 +1,22 @@
+"""
+ScrapyAPI scrape some stuff.
+
+Copyright (C) 2026  Kanye Sue
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from celery import Celery
+
+celery_app = Celery("tasks", broker='redis://localhost:6379/0', backend='redis://localhost:6379/1')

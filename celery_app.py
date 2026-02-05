19c7390b0fdec46f795dbe267bb741da2bbb6af0
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2026-02-05 15:00:43 +0630

    use sys nginx

--- celery_app.py
+++ celery_app.py
@@ -21,8 +21,8 @@ from celery import Celery
 
 celery_app = Celery(
     "tasks",
-    broker='redis://redis:6379/0',
-    backend='redis://redis:6379/1'
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/1'
 )
 
 celery_app.conf.result_expires = 600
